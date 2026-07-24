6d6a74cf9d5ea67b07bbad6781be85c8cf0beed8
liliko73 <kusuli720@gmail.com>
2026-07-24 22:56:21 +0900

    Fix: SMTPの設定をPort 587に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   # ResendのSMTP設定
   config.action_mailer.smtp_settings = {
     address:              'smtp.resend.com',
-    port:                 465,
+    port:                 587,
     user_name:            'resend',
     password:             ENV['RESEND_API_KEY'],
     authentication:       :plain,
-    ssl:                  true
+    enable_starttls_auto: true
   }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
