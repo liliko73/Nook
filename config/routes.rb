f10c1064140314577e3236036f8d8ad16d9ae6b9
liliko73 <kusuli720@gmail.com>
2026-08-20 14:35:46 +0900

    feat: プロフィール編集機能（ユーザー自身）の実装
    - ProfileControllerにedit, updateアクションを実装
    - プロフィール編集画面およびマイページからの遷移リンクを作成
    - ストロングパラメータの設定（username, self_introduction, birthday, gender, prefecture）
    - 編集フォームのバリデーションエラーメッセージ表示および更新処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "profiles/show"
+  get "profiles/edit"
+  get "profiles/update"
   get "themes/show"
   devise_for :users, controllers: {
     registrations: "users/registrations"
@@ -21,6 +24,9 @@ Rails.application.routes.draw do
   resources :questions, only: %i[ index show new create edit update destroy ] do
     resources :answers, only: %i[ create destroy ], shallow: true
   end
+
+  # マイページ・プロフィール編集用
+  resource :profile, only: %i[ show edit update ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
